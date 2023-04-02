f7ce42e3786df10ded5ab2eb1c1aeeb9e58cd5b0
Zachary <zachary@Zipcoders-MBP-2.fios-router.home>
2023-04-02 15:23:39 -0400

    wheels spun and needed to get help from a class mate. i think have a good understanding. saving getMostCommon for myself without help

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,55 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    T[] inputArray;
+
+    public ArrayUtility(T[] inputArray){
+        this.inputArray = inputArray;
+    }
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        int count = 0;
+        for(T t : arrayToMerge){
+            if(t.equals(valueToEvaluate)){
+                count++;
+            }
+        }
+        for(T t : inputArray){
+            if(t.equals(valueToEvaluate)){
+                count++;
+            }
+        }
+    return count;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        int count = 0;
+        for (T t : inputArray){
+            if(t.equals(valueToEvaluate)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        T[] newArray = Arrays.copyOf(inputArray, inputArray.length-getNumberOfOccurrences(valueToRemove));
+        int i = 0;
+        for (T t : inputArray){
+            if (t != valueToRemove){
+                newArray[i] = t;
+                i++;
+            }
+        }
+        return newArray;
+    }
 }
